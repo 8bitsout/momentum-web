6b527cf6d9e8e47c8a47fbcc842857190a918d50
8bitstout <conor.murphy4304@gmail.com>
2020-08-14 15:11:06 -0400

    add route for globalx

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from Nasdaqomx import Nasdaqomx
+from globalxetfs import GlobalXEtfs
 import yfinance as yf
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -6,28 +7,42 @@ from flask import Flask
 from typing import NamedTuple, List
 
 app = Flask(__name__)
+gxe = GlobalXEtfs()
+gxe.get_etfs()
 
 
-@app.route('/api/momentum/tickers/<index>')
-def momentum_tickers(index: list):
-    top, bottom = fetch_momentum_stocks(index)
+@app.route('/api/momentum/nasdaqomx/tickers/<index>')
+def momentum_tickers(index):
+    ndx = Nasdaqomx()
+    tickers = ndx.fetchTickers(index)
+    top, bottom = fetch_momentum_stocks(tickers)
     print(top)
     return {"best": top, "worst": bottom}
 
 
-def fetch_momentum_stocks(index, start_date=datetime.today(), lookback_period=6):
+@app.route('/api/momentum/globalx/tickers/<index>')
+def globalx_momentum_tickers(index):
+    print(index)
+    holdings = gxe.etfs_map[index].get_holdings()
+    print(holdings)
+    tickers = holdings["Ticker"].tolist()
+    print(tickers)
+    top, bottom = fetch_momentum_stocks(tickers)
+    print(top)
+    return {"best": top, "worst": bottom}
+
+
+def fetch_momentum_stocks(tickers, start_date=datetime.today(), lookback_period=6):
     def get_my_key(obj):
         return obj['return']
 
-    ndx = Nasdaqomx()
-    tickers = ndx.fetchTickers(index)
     end_date = start_date - relativedelta(months=lookback_period)
     stocks = yf.download(tickers, end_date, start_date)
-
+    stocks.dropna()
     stock_returns = []
-
+    print(stocks)
     for stock in stocks['Adj Close']:
-        s = stocks['Adj Close'][stock]
+        s = stocks['Adj Close'][stock].dropna()
         r = ((s[-1] / s[0]) - 1) * 100
         stock_returns.append({'name': s.name, 'return': r})
 
